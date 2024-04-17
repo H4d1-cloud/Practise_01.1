0e972e4493709989a086f8ca6b84c60b318c7e60
hadi.hussain <hadi.hussain@elev.ga.ntig.se>
2024-04-17 10:36:13 +0200

    Initial commit

---
+++ src/Practise_01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Practise_01 {
+    public static void main(String[] args){
+        if (6 >=3) {
+            System.out.println("Hello world!");
+        }
+    }
+}
